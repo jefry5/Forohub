708c4119a40a0c250cf402eff27c2f039c96f324
jefry5 <jefrymarquez614@gmail.com>
2025-01-15 14:53:46 -0500

    Initial Commit

---
+++ src/main/java/com/alurachallenge/forohub/ForohubApplication.java
@@ -0,0 +1,14 @@
+package com.alurachallenge.forohub;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForohubApplication implements CommandLineRunner {
+
+	public static void main(String[] args){SpringApplication.run(ForohubApplication.class, args);}
+
+	@Override
+	public void run(String... args) throws Exception {}
+}
